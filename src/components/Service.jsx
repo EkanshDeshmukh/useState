d9f726476e60a751a4817d5c0451afa16975be23
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-03-28 10:55:31 +0530

    Understanding of Mounted and UnMounted in UseEffect

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from '../utils/Axios';
 
 function Service() {
-    useEffect(() => {
-        console.log('Service components is Created !!');
-        return () => {
-            console.log('Service components is deleted !!');
-        }
-    })
+  const [first, setFirst] = useState("this is a First");
+  const [Second, setSecond] = useState("this is a Second");
 
-    return (
-        <div>Service</div>
-    )
+  useEffect(() => {
+    console.log('Component is created');
+
+   
+    axios.get('/users')
+      .then(users => console.log(users))
+      .catch(err => console.log(err));
+
+    
+    return () => {
+      console.log('Component is destroyed');
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>{first}</h1>
+      <button className='px-2 py-2 bg-red-300 rounded' onClick={() => setFirst("this is a First Updated")}>Update First</button>
+      <h1>{Second}</h1>
+      <button className='px-2 py-2 bg-red-300 rounded' onClick={() => setSecond("this is a Second Updated")}>Update Second</button>
+    </div>
+  );
 }
 
-export default Service
+export default Service;
--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../utils/Axios'
 
 function Show() {
+
+  const [product, setproduct] = useState([])
+  const getProduct = () => {
+    axios.get('/products')
+      .then(product => setproduct(product.data))
+      .catch(err => console.log(err));
+  };
+  useEffect(() => {
+    getProduct()
+  }, [])
+
   return (
-    <div>Show</div>
+    <div>
+     <button className='px-3 py-2 bg-red-200 rounded'>Products</button>
+      <ul>
+        {product.map(product => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
